5e721a7dceecfd864512348c8f3b27929da1a398
Joe Devietti <devietti@cis.upenn.edu>
2015-09-13 11:26:07 -0400

    refactor Uop class * move Enum's to top-level classes * replace 4-arg ctor with an all-arg ctor instead

--- src/main/java/cis501/submission/UopFactory.java
+++ src/main/java/cis501/submission/UopFactory.java
@@ -1,7 +1,6 @@
 package cis501.submission;
 
-import cis501.IUopFactory;
-import cis501.Uop;
+import cis501.*;
 
 /** If you subclass cis501.Uop, update this factory to generate instances of your subclass */
 public class UopFactory implements IUopFactory {
@@ -12,8 +11,12 @@ public class UopFactory implements IUopFactory {
     }
 
     @Override
-    public Uop create(int sr1, int sr2, int dr, Uop.MemoryOp mop) {
-        return new Uop(sr1, sr2, dr, mop);
+    public Uop create(int sr1, int sr2, int dr, MemoryOp mop, int uid, long pc,
+                      Flags f, Direction dir, long immed, long dataAddr, long fallthruPC,
+                      long targetPC, String macro, String micro) {
+        return new Uop(sr1, sr2, dr, mop, uid, pc,
+        f, dir, immed, dataAddr,
+        fallthruPC, targetPC, macro, micro);
     }
 
 }
